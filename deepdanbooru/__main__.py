cc7bb48a3a004f2c65a7da564542a000594f6376
onTDB <github@ontdb.com>
2023-01-22 01:58:38 +0900

    Version 1.1.3

--- deepdanbooru/__main__.py
+++ deepdanbooru/__main__.py
@@ -7,7 +7,7 @@ import os
 import deepdanbooru as dd
 import tensorflow.lite as tflite
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 
 
 @click.version_option(prog_name="DeepDanbooru", version=__version__)
